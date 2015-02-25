fad546afad3b00e02a6db1d0caa37cd8e3468cc3
setkeh <setkeh@gmail.com>
2015-02-25 13:16:11 +1100

    Added Instance Deployment code and Makefile to Deploy the instances on EC2

---
+++ Deploy/instances.js
@@ -0,0 +1,39 @@
+var AWS = require('aws-sdk'); 
+var ec2 = new AWS.EC2({region: 'ap-southeast-2', maxRetries: 15});
+var config = require('./config.json');
+var params = config.params;
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+  if (err) { console.log("Could not create instance", err); return; }
+
+  var instanceIdProxy = data.Instances[0].InstanceId;
+  console.log("Created Proxy instance", instanceIdProxy);
+
+  var InstanceIdNode1 = data.Instances[1].InstanceId;
+  console.log("Created App Node1 Instance", InstanceIdNode1);
+
+  var InstanceIdNode2 = data.Instances[2].InstanceId;
+  console.log("Created App Node1 Instance", InstanceIdNode2);
+
+  // Add tags to the instances
+  ParamsProxy = {Resources: [instanceIdProxy], Tags: [
+    {Key: 'Name', Value: config.tags.instance1Name}
+  ]};
+ ParamsNode1 = {Resources: [InstanceIdNode1], Tags: [
+    {Key: 'Name', Value: config.tags.instance2Name}
+  ]};
+ ParamsNode2 = {Resources: [InstanceIdNode2], Tags: [
+    {Key: 'Name', Value: config.tags.instance3Name}
+  ]};
+
+  ec2.createTags(ParamsProxy, function(err) {
+    console.log("Tagging instance", err ? "failure" : "success");
+  });
+  ec2.createTags(ParamsNode1, function(err) {
+    console.log("Tagging instance", err ? "failure" : "success");
+  });
+  ec2.createTags(ParamsNode2, function(err) {
+    console.log("Tagging instance", err ? "failure" : "success");
+  });
+});
--- README.md
+++ README.md
@@ -8,3 +8,33 @@ I have Added a basic Nginx config for load balancing and reverse proxy but there
 This is the Upstream puppet module.
 
 Adding that to the ToDo list.
+
+EC2 Instance creation script is working but needs some manual intervention for the EC2 Node.js library namely it needs an API key to do this create the file,
+
+	~/.aws/credentials
+
+And add the following data,
+
+	[default]
+	aws_access_key_id = <YOUR_ACCESS_KEY_ID>
+	aws_secret_access_key = <YOUR_SECRET_ACCESS_KEY>
+
+If you need to use Multiple API keys you can follow,
+
+	http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html#Using_Profiles_with_the_SDK
+
+To setup api profiles.
+
+API Profiles will Require the following modification to '''instances.js'''
+
+	var credentials = new AWS.SharedIniFileCredentials({profile: 'ProfileName'});
+	AWS.config.credentials = credentials;
+
+there is probably a better way of doing than hardcoding it in the JS and i shall add it to the todo list.
+
+
+# To-Do
+My List of To-Do's to improve the current code.
+
+  * Nginx Puppet module to setup the Proxy and Load balancer according to the upstrem module
+  * Find the best way of implimenting AWS key profiles instead f hardcoding the config in the JS
