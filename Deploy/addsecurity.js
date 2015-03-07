e257dd520b16fd617081e3cc3c53a32508341038
setkeh <setkeh@gmail.com>
2015-03-07 18:58:39 +1100

    Added some corrections to Makefile and addsecurity.js

--- Deploy/addsecurity.js
+++ Deploy/addsecurity.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 var ec2 = new AWS.EC2({region: 'ap-southeast-2', maxRetries: 15});
-var config = require('./config';)
+var config = require('./config');
 
 ec2.authorizeSecurityGroupIngress(config.AddSecurity, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
